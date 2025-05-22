13669d33efceac9061189c6ba8ec272f6040744d
neycha <regentmarysapro@gmail.com>
2025-05-22 22:43:01 +0200

    Fix TypeScript build for Vercel

--- backend/src/errorHandling/resultPattern.ts
+++ backend/src/errorHandling/resultPattern.ts
@@ -1,33 +1,50 @@
-export type Result<T, E extends Error> = Ok<T, E> | Err<T, E>;
+export abstract class Result<T, E> {
+  abstract isOk(): this is Ok<T, E>;
+  abstract isErr(): this is Err<T, E>;
+  abstract value: T;
+  abstract error: E;
+}
 
-export class Ok<T, E> {
-  private constructor(public readonly value: T) {}
+export class Ok<T, E> extends Result<T, E> {
+  constructor(public value: T) {
+    super();
+  }
+
+  isOk(): this is Ok<T, E> {
+    return true;
+  }
+
+  isErr(): this is Err<T, E> {
+    return false;
+  }
+
+  get error(): E {
+    throw new Error("Cannot get error from Ok");
+  }
 
   static of<T, E>(value: T): Ok<T, E> {
     return new Ok(value);
   }
+}
 
-  public isOk(): this is Ok<T, E> {
+export class Err<T, E> extends Result<T, E> {
+  constructor(public error: E) {
+    super();
+  }
+
+  isOk(): this is Ok<T, E> {
+    return false;
+  }
+
+  isErr(): this is Err<T, E> {
     return true;
   }
 
-  public isErr(): this is Err<T, E> {
-    return false;
+  get value(): T {
+    throw new Error("Cannot get value from Err");
   }
-}
-
-export class Err<T, E> {
-  private constructor(public readonly error: E) {}
 
   static of<T, E>(error: E): Err<T, E> {
     return new Err(error);
-  }
-
-  public isOk(): this is Ok<T, E> {
-    return false;
-  }
-
-  public isErr(): this is Err<T, E> {
-    return true;
   }
 }
